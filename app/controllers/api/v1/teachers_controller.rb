66f16924a41b0024a2b4a542a7af8aa97ffe00c0
dongmy54 <615760433@qq.com>
2017-04-12 13:04:40 +0800

    complete delete '/teachers/:id' json

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -17,4 +17,23 @@ class Api::V1::TeachersController < ApiController
             }
 		}
 	end
+
+
+	def destroy
+		@teacher = Teacher.find(params[:id])
+        
+         @teacher.courses.each do |course|
+              @teacher.courses.delete(course)  
+         end
+        
+        #@teacher.courses.destroy   不能这样写
+        # 我们需要达到的目的是：让课程中记录的teacher_id为nil
+
+		@teacher.destroy
+
+		render :json => {
+			:message => "此老师及其代课信息已全部删除"
+		}
+	end
+
 end
